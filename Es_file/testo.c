b9aa3d8dee524ccfc13c0d447cf9d0fd836cc478
AlbertoPavarin <albertopavarin@libero.it>
2021-10-22 23:17:48 +0200

    Implementazione controllo stringhe

---
+++ Es_file/testo.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    int contatore = 0;
+    char *indicatore = "";
+    if (argc != 5 || strcmp(argv[3], "-c") != 0 && strcmp(argv[3], "-p") != 0)
+    {
+        printf("Uso: $ ./prg.out input.txt output.txt modalita'(-c, -p) cosa cercare\n\n");
+        return 1;
+    }
+
+    FILE *input = fopen(argv[1], "r");
+    FILE *output = fopen(argv[2], "w");
+    if (input == NULL)
+    {
+        printf("File inesistente\n\n");
+        return 1;
+    }
+
+    if (output == NULL)
+    {
+        printf("Errore\n\n");
+        return 1;
+    }
+
+    struct stat sb;
+    stat(argv[1], &sb);
+    char *contenutoFile = malloc(sb.st_size);
+    if (contenutoFile == NULL)
+    {
+        printf("Error\n\n");
+        return 1;
+    }
+
+    while (fscanf(input, "%s", contenutoFile) != EOF)
+    {
+        if (strcmp(argv[3], "-c") == 0)
+        {
+            if (strcmp(contenutoFile, argv[4]) == 0)
+                contatore++;
+            
+            indicatore = "Il carattere";
+        }
+        else if (strcmp(argv[3], "-p") == 0)
+        {
+            if (strcmp(contenutoFile, argv[4]) == 0)           
+                contatore++;
+
+            indicatore = "La stringa";           
+        }
+    }
+
+    fprintf(output, "%s %s compare: %d volte", indicatore, argv[4], contatore);
+    printf("Valore salvato nel file %s\n\n", argv[2]);
+    fclose(input);
+    fclose(output);
+    free(contenutoFile);
+    return 0;
+}
