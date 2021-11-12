8d08175e8299c0b06d30e2a285e153a16ed205df
AlbertoPavarin <albertopavarin@libero.it>
2021-11-12 22:35:31 +0100

    Aggiunta funzione per stampare a schermo la rubrica e per aggiungere un contatto alla rubrica, sovrascrivendo il file gia' esistente

--- rubrica/rubrica.c
+++ rubrica/rubrica.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+typedef struct  
 {
     char nome[50];
     char cognome[50];
@@ -12,11 +12,58 @@ typedef struct
 
 void mostraRubrica(char contenutoFile[100][100], int lung)
 {
-    for (int i = 0; i < lung; i++)
+    printf("La tua rubrica e':\n");
+    for (int i = 1; i <= lung; i++)
     {
-        printf("%s", contenutoFile[i]);        
+        printf("%s ", contenutoFile[i - 1]);       
+        if (i%3 == 0)
+            printf("\n");
     }
     printf("\n");
+}
+
+void aggiuntaContatto(FILE *rubrica, char contenutoFile[100][100], int lung)
+{
+    int cont;
+    char numeri[100][100];
+    printf("Numeri gia' esistenti:\n");
+    for (int i = 1; i <= lung; i++)
+    {
+        if (i % 3 == 0)
+        {
+            strcpy(numeri[i-1], contenutoFile[i-1]);
+            printf("%s\n", numeri[i - 1]);
+            cont += 3;
+        }
+    }
+    Persona_T persona;
+    printf("\nInserisci il nome della persona che desideri inserire nella rubrica\n");
+    scanf("%s", persona.nome);
+
+    printf("Inserisci il cognome di %s\n", persona.nome);
+    scanf("%s", persona.cognome);
+
+    do
+    {
+        printf("Inserisci il numero di %s %s\n", persona.nome, persona.cognome);
+        scanf("%s", persona.numero);
+    }
+    while (strlen(persona.numero) != 10);
+    
+    for (int i = 0; i <= cont; i++)
+    {
+        if (strcmp(numeri[i+2], persona.numero) == 0)
+        {
+            printf("Numero gia' esistente\n");
+            exit(2);
+        }
+    }
+    fprintf(rubrica, "\n%s %s %s", persona.nome, persona.cognome, persona.numero);
+}
+
+void eliminaContatto(FILE *input, char contenutoFile[100][100], int lung)
+{
+    //Da implemantare
 }
 
 int main(int argc, char *argv[])
@@ -28,36 +75,38 @@ int main(int argc, char *argv[])
         printf("Errore negli argomenti\n");
         exit(0);
     }
-    FILE *input = fopen(argv[1], "r+");
-    if (input == NULL)
+    FILE *rubrica = fopen(argv[1], "r+");
+    if (rubrica == NULL)
     {
         printf("Il file in input non esiste\n");
         exit(1);
     }
 
     char contenutoFile[100][100];
-    Persona_T persona[100];
-    while(fscanf(input, "%s", contenutoFile[lunghezza]) != EOF)
+    while(fscanf(rubrica, "%s", contenutoFile[lunghezza]) != EOF)
     {
         lunghezza++;
     }
-    fclose(input);
 
     printf("Premi 1 per mostrare la rubrica a video\nPremi 2 per aggiungere un contatto alla rubrica\nPremi 3 per eliminare un contatto dalla rubrica dopo averlo ricercato con il suo numero di telefono\n\n");
     scanf("%d", &scelta);
+    printf("\n");
     switch (scelta)
     {
         case 1:
-            mostraRubrica(contenutoFile, sizeof(input));
+            mostraRubrica(contenutoFile, lunghezza);
             break;
         case 2:
-            printf("sus2\n");
+            // Implementare scrittura su nuovo file
+            aggiuntaContatto(rubrica, contenutoFile, lunghezza);
             break;
-        case 3:
-            printf("sus3\n");
+        case 3: 
+            // Implementare completamente
+            eliminaContatto(rubrica, contenutoFile, lunghezza);
             break;
         default:
             printf("Errore\n");
             break;
     }
+    fclose(rubrica);
 }
