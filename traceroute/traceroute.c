73a0c232eb8548e82cdd1f2b884ee7dcfd8bbaf2
xAlbi <72099114+AlbertoPavarin@users.noreply.github.com>
2022-03-04 12:26:31 +0100

    inizio esercizio 7

---
+++ traceroute/traceroute.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+
+int main()
+{
+    int pid1, pid2, pid3, p1p2[2], p2p3[2], p3p0[2];
+    double totale = 0;
+    char buff[1], *ptr, strSec[100];
+
+    if (pipe(p1p2) < 0)
+    {
+        printf("Errore nella creazione della pipe\n");
+        exit(0);
+    }
+
+    pid1 = fork();
+    if (pid1 == 0)
+    {
+        close(p1p2[0]);
+        close(1);
+        dup(p1p2[1]);
+        close(p1p2[1]);
+        execl("/bin/cat", "cat", "file.txt", NULL);
+        return -1;
+    }
+
+    if (pipe(p2p3) < 0)
+    {
+        printf("Errore nella creazione della pipe\n");
+        exit(1);
+    }
+
+    pid2 = fork();
+    if (pid2 == 0)  
+    {
+        close(p1p2[1]);
+        close(0);
+        dup(p1p2[0]);
+        close(p1p2[0]);
+
+        close(p2p3[0]);
+        close(1);
+        dup(p2p3[1]);
+        close(p2p3[1]);
+        execl("/bin/awk", "awk", "{print $4}", NULL);
+        return -1;
+    }
+
+    if (pipe(p3p0) < 0)
+    {
+        printf("Errore nella creazione della pipe");
+        exit(2);
+    }
+
+    pid3 = fork();
+    if (pid3 == 0)
+    {
+        close(p1p2[0]);
+        close(p1p2[1]);
+        close(p2p3[1]);
+        close(0);
+        dup(p2p3[0]);
+        close(p2p3[0]);
+
+        close(p3p0[0]);
+        //close(1);
+        //dup(p3p0[1]),
+        close(p3p0[1]);
+
+        execl("/bin/tail", "tail", "-n", "+2", NULL);
+        return -1;
+    }
+
+    close(p1p2[0]);
+    close(p1p2[1]);
+    close(p2p3[0]);
+    close(p2p3[1]);
+    close(p3p0[1]);
+    
+    fflush(stdout);
+    while (read(p3p0[0], buff, 1) > 0)
+    {
+        strncat(strSec, &buff[0], sizeof(buff[0]));
+        if (buff[0] == '\n' || buff[0] == '*')
+        {
+            printf("numero ricevuto %s\n", strSec);
+            totale += strtod(strSec, &ptr);
+            strSec[0] = '\0';
+        }
+    }
+
+    close(p3p0[0]);
+    printf("Totale: %.2lf ms\n", totale);
+    return 0;
+}
