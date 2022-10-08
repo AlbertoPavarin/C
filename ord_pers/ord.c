165fac508335f4e28e002aca286a7b0b8022a3f1
AlbertoPavarin <albertopavarin@libero.it>
2022-10-08 10:10:56 +0200

    Fine

--- ord_pers/ord.c
+++ ord_pers/ord.c
@@ -4,14 +4,30 @@
 
 #define DIM 100
 
+void ordina(int array[], int inizio, int fine)
+{
+    for (int i = inizio; i < fine - 1; i++)
+    {
+        for (int j = inizio; j < fine - 1; j++)
+        {
+            if (array[j] > array[j+1])
+            {
+                int tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+        }
+    }
+}
+
 int main()
 {
-    int numeri[DIM], rdm = 0, pariCnt = 0, dispCnt = 0;
+    int numeri[DIM], rdm = 0, pariCnt = 0, dispCnt = 0, controlloDispari = 0, controlloPari = 0, cntNum2 = 0, k = 0;
     srand(time(NULL));
 
     for (int i = 0; i < DIM; i++)
     {
-        rdm =  1 + rand() % 200;
+        rdm =  1 + rand() % 233;
         for (int j = 0; j <= i; j++)
         {
             if (rdm == numeri[j])
@@ -21,96 +37,74 @@ int main()
             }
             if (j == i)
             {
+                if ((rdm % 2) == 0)
+                {
+                    pariCnt++;
+                }
+                else
+                {
+                    dispCnt++;
+                }
                 numeri[i] = rdm;
             }
         }
     }
 
-    for (int i = 0; i < DIM; i++)
-    {
-        if ((numeri[i]%2) == 0)
-            printf("%d ", numeri[i]);
-    }
-
-    printf("\n\n");
-
-    for (int i = 0; i < DIM; i++)
-    {
-        if ((numeri[i]%2) != 0)
-            printf("%d ", numeri[i]);
-    }
-
-    for (int i = 0; i < DIM; i++)
-    {
-        if ((numeri[i] % 2) == 0)
-        {
-            pariCnt++;
-        }
-        else
-        {
-            dispCnt++;
-        }
-    }
-
     int dispari[dispCnt];
+    int pari[pariCnt];
 
     pariCnt = (pariCnt > 20) ? 20 : pariCnt;
 
     dispCnt = (dispCnt > 20) ? 20 : dispCnt;
 
+    for (int i = 0; i < DIM; i++)
+    {
+        if ((numeri[i]%2) == 0)
+        {
+            pari[controlloPari] = numeri[i];
+            controlloPari++;
+        }
+        else if ((numeri[i]%2) != 0)
+        {
+            dispari[controlloDispari] = numeri[i];
+            controlloDispari++;
+        }
+    }
+
+    ordina(pari, 0, controlloPari);
+    ordina(dispari, 0, controlloDispari);
+
+    for (int i = pariCnt; i < controlloPari; i++)
+    {
+        numeri[pariCnt + dispCnt + k] = pari[i];
+        k++;
+    }
+
+    for (int i = dispCnt; i < controlloDispari; i++)
+    {
+        numeri[pariCnt + dispCnt + k] = dispari[i];
+        k++;
+    }
+
 
     for (int i = 0; i < pariCnt; i++)
     {
-        for (int j = i; j < DIM; j++)
-        {
-            if ((numeri[j] % 2) == 0)
-            {
-                int tmp = numeri[j];
-                numeri[j] = numeri[i];
-                numeri[i] = tmp;
-                break;
-            }
-        }
+        numeri[i] = pari[i];
     }
 
-    for (int i = pariCnt; i < pariCnt + dispCnt; i++)
+    for (int i = 0; i < dispCnt; i++)
     {
-        for (int j = pariCnt; j < pariCnt + dispCnt; j++)
-        {
-            if (numeri[j] > numeri[j+1])
-            {
-                int tmp = numeri[j];
-                numeri[j] = numeri[j+1];
-                numeri[j+1] = tmp;
-            }
-        }
+        numeri[dispCnt+i] = dispari[i];
     }
 
-    /*for (int i = 0; i < DIM - 1; i++)
-    {
-        for (int j = 0; j < DIM - 1; j++)
-        {
-            if (numeri[j] > numeri[j+1])
-            {
-                int tmp = numeri[j];
-                numeri[j] = numeri[j+1];
-                numeri[j+1] = tmp;
-            }
-        }
-    }*/
+    ordina(numeri, dispCnt+pariCnt, DIM);
 
-
-    printf("\n\n\n\n\n");
-
-    for (int i = 0; i < pariCnt; i++)
+    for (int i = 0; i < DIM; i++)
     {
         printf("%d ", numeri[i]);
     }
-printf("\n\n\n\n\n");
-    for (int i = pariCnt; i < pariCnt + dispCnt; i++)
-    {
-        printf("%d ", numeri[i]);
-    }
+
+    printf("\n");
 
     return 0;
 }
