aa30e92c6ebb4368fbe55308ba1c4bb5da55e965
AlbertoPavarin <albertopavarin@libero.it>
2022-11-02 08:36:19 +0100

    es

--- socket/es2/client.c
+++ socket/es2/client.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/types.h>
+#include <netinet/in.h>//definiscono la struttura degli indirizzi e funzione del tipo ntohl
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
 #include <unistd.h>
 
-#define DIM 256
-#define SERVER_PORT 55555
+#define DIM 512
+#define SERVERPORT 1313
 
-int main()
-{
+
+
+
+int main(int argc,char** argv)
+{   //creazione di un elemento di tipo sockaddr vedi pagina 162
     struct sockaddr_in servizio;
-    int socketfd;
-    char str[DIM], str_in[DIM];
-
-    servizio.sin_family = AF_INET;
-    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
-    servizio.sin_port = htons(SERVER_PORT);
-
-    socketfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio));
-
-    printf("Inserisci stringa:\n");
-    scanf("%s", str);
-
-    write(socketfd, str, sizeof(str));
-
-    read(socketfd, str_in, sizeof(str_in));
-
-    printf("Stringa ricevuta dal server: %s\n", str_in);
+    int socketfd;// identificatore della socket
+    char str1[DIM];
+    //inizializzazione dell'elemento di tipo sockaddr
+    servizio.sin_family=AF_INET;
+    servizio.sin_addr.s_addr=htonl(INADDR_ANY);
+    servizio.sin_port=htons(SERVERPORT);
+    //creazione e definizione del Socket di tipo stream tcp pg 163
+    socketfd=socket(AF_INET,SOCK_STREAM,0);
+    //connessione al server
+    connect(socketfd,(struct sockaddr*)&servizio,sizeof(servizio));
+    
+    printf("Inserisci la stringa\n");
+    scanf("%s",str1);
+    write(socketfd,str1,sizeof(str1));
+    read(socketfd,str1,sizeof(str1));
+    printf("Stringa ricevuta dal server invertita\n %s",str1);
     
     close(socketfd);
 
--- socket/es2/server.c
+++ socket/es2/server.c
@@ -1,58 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#include <unistd.h>
+#include <netdb.h>
 #include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
 
-#define DIM 256
-#define SERVER_PORT 55555
+#define DIM 512
+#define SERVERPORT 1313
 
-void inverti(char str[], int len, char new_str[])
+void inverti(char str[], char newstr[])
 {
-    int j = 0;
-    for (int i = len; i >=0; i--)
-    {
-        new_str[j++] = str[i];
-    }
+    newstr[0] = '\0';
+	int i, j = 0;
+	for (i = strlen(str) - 1; i >= 0; i--)
+	{
+		newstr[j] = str[i];
+		j++;
+	}
+	
 }
+
+
+
+
 
 int main()
 {
-    struct sockaddr_in servizio, addr_remoto;
-    int socketfd, soafd, fromlen = sizeof(servizio); 
-    char str[DIM], new_str[DIM];
-
-    // dati socket
-    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
-    servizio.sin_family = AF_INET;
-    servizio.sin_port = htons(SERVER_PORT);
-
-    // transport endpoint, creazione socket
-    socketfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio));
-
-    listen(socketfd, 10);
-
-    while(1)
+    struct sockaddr_in servizio, addr_remoto;// record con i dati del server e del client
+    int socketfd, soa, fromlen=sizeof(servizio);
+    char str[DIM],newstr[DIM];
+    // definizione dei dati del socket 
+    servizio.sin_family=AF_INET;
+    servizio.sin_addr.s_addr=htonl(INADDR_ANY);
+    servizio.sin_port=htons(SERVERPORT);
+    // impostazione del transport endpoint del server
+    socketfd=socket(AF_INET,SOCK_STREAM,0);
+    // leghiamo l'indirizzo al transport endpoint
+    bind(socketfd,(struct sockaddr*)&servizio,sizeof(servizio));
+    // poniamo il server in ascolto delle richieste dei client 
+    listen(socketfd,10);// può attendere fino a 10 client
+    // ciclo infinito
+    for (; ;)
     {
-        printf("Server in ascolto\n");
+        printf("\n\nServer in ascolto...");
 
         fflush(stdout);
 
-        soafd = accept(socketfd, (struct sockaddr*)&servizio, &fromlen);
+        soa=accept(socketfd,(struct sockaddr*)&addr_remoto,&fromlen);
+        //legge dal client
+        read(soa,str,sizeof(str));
+        
+        printf("Stringa ricevuta: %s\n",str);
+        
+        inverti(str,newstr);
+        
+        printf("Stringa invertita: %s\n",newstr);
+        
+        write(soa, newstr, strlen(newstr));
+        
+        close(soa);
 
-        read(soafd, str, sizeof(str));
-
-        inverti(str, strlen(str) - 1, new_str);
-
-        printf("Messaggio ricevuto: %s\n\n\n", str);
-
-        write(soafd, new_str, strlen(new_str));
-
-        close(soafd);
-    }
-
-    return 0;
+     }
+      
+   return 0;
+    
 }
