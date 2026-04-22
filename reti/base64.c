07ed2f20a4f2b455898b9ddfe95285a7fe58fcc9
xAlbi <albertopavarin@libero.it>
2026-04-22 22:51:22 +0200

    funziona in ogni caso

--- reti/base64.c
+++ reti/base64.c
@@ -15,22 +15,19 @@ int main(){
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = 0;
     
-    printf("%d\n", strlen(s)*sizeof(char));
     len = strlen(s);
-    int dimbased = len*4/3;
-    char based[dimbased]; // spazio calcolato con il rapporto 4/3 ---> 50*4/3
-    carry = len % 3;
-    
-    if (carry == 0) {
-        for (int i = 0; i < len;){
-            char b0 = s[i++];
-            char b1 = s[i++];
-            char b2 = s[i++];
-            based[j++] = b64_table[(b0 >> 2)];
-            based[j++] = b64_table[((b0 & 3)<<4)|(b1>>4)];
-            based[j++] = b64_table[((b1 & 15)<<2)|(b2>>6)];
-            based[j++] = b64_table[b2 & 63];
-        }
+    carry = len % 3;    
+    int dimbased = (carry == 0) ? len*4/3 :  len*4/3+(4-carry);
+    printf("dim: %d\n", dimbased);
+    char based[dimbased]; // spazio calcolato con il rapporto 4/3 e un appropriato padding
+    for (int i = 0; i < len;) {
+        char b0 = s[i++];
+        char b1 = (i < len) ? s[i++] : 0;
+        char b2 = (i < len) ? s[i++] : 0;
+        based[j++] = b64_table[(b0 >> 2)];
+        based[j++] = b64_table[((b0 & 3)<<4)|(b1>>4)];
+        based[j++] = (b1 == 0) ? '=' : b64_table[((b1 & 15)<<2)|(b2>>6)];
+        based[j++] = (b2 == 0) ? '=' : b64_table[b2 & 63];
     }
 
     for (int i = 0; i < dimbased; i++)
